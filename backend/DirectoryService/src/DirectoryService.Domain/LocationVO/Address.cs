8bba0bec7427d99ac435f0fe95c4b9cc2b9f0a6b
k.nikolaev <k.nikolaev@ozm.ru>
2026-09-03 08:16:07 +0300

    add new paramether

--- backend/DirectoryService/src/DirectoryService.Domain/LocationVO/Address.cs
+++ backend/DirectoryService/src/DirectoryService.Domain/LocationVO/Address.cs
@@ -3,16 +3,17 @@
 public sealed record Address
 {
     public string Value { get; }
-    private Address(string value)
+
+    private Address(string country, string city, string street)
     {
-        Value = value;
+        Value = $"{country}, {city}, {street}";
     }
-    public static Address Create(string value)
+    public static Address Create(string country, string city, string street)
     {
-        if (string.IsNullOrWhiteSpace(value))
+        if (string.IsNullOrWhiteSpace(street) || string.IsNullOrWhiteSpace(city) || string.IsNullOrWhiteSpace(country))
         {
-            throw new ArgumentException("Address cannot be empty.", nameof(value));
+            throw new ArgumentException("Address components cannot be empty.", nameof(street));
         }
-        return new Address(value);
+        return new Address(country, city, street);
     }
 }
